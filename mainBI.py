07f044bcb1095cfb656355a1f87a69adcafe7df0
William Fauriat <180698994+wfauriat@users.noreply.github.com>
2025-06-25 15:58:47 +0200

    completed OOP

--- mainBI.py
+++ mainBI.py
@@ -1,162 +1,154 @@
 #%%
 from pyBI.base import UnifVar, InvGaussVar, ObsVar
-from pyBI.inference import InfAlgo, MHalgo
+from pyBI.inference import MHalgo, MHwGalgo
 
 import numpy as np
 import scipy.stats as sst
 
 import matplotlib.pyplot as plt
 
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, WhiteKernel
+
+
 np.random.seed(123)
 
 #%%
 
-def modeltrue(x,b):
-    return b[0] + b[1]*x[:,0] + b[2]*x[:,0]**2 + 1*x[:,1]
+case = 0
+# case = 1
 
-def modelfit(x,b):
-    return b[0] + b[1]*x[:,0] + b[2]*x[:,0]**2
-
-
-b0 = [2, -1, 2, 0]
-nslvl = 0.2
-nsp1 = 0.1
-biasp1 = -1
-
-xplot = np.repeat(np.c_[np.linspace(0,6,50)],2, axis=1)
-xplot[:,1] = 1
-
-xmes = np.hstack([np.c_[[0, 0.5, 1, 2, 2.5, 2.8, 4, 4.4, 5.2, 5.5]],
-                 biasp1+nsp1*np.c_[np.random.randn(10)]])
-ymes = modeltrue(xmes, b0)
-ymes += np.random.randn(xmes.shape[0])*nslvl
-
+inftype = 'MH'
+# inftype = 'MHwG'
 
 #%%
 
-Ndim = 3
-pl = -3
-ph = 3
-sinvg = [0.4, 0.3, 1]
-# sinvg = [0.05, 0.01, 10]
+if case == 0:
+    def modeltrue(x,b):
+        return b[0] + b[1]*x[:,0] + b[2]*x[:,0]**2 + 1*x[:,1]
 
-# sm = np.array([0.2]*Ndim)
-sm = [0.4, 0.4, 0.05]
-smexp = 0.1
+    def modelfit(x,b):
+        return b[0] + b[1]*x[:,0] + b[2]*x[:,0]**2
 
-covProp = np.eye(3)*1e-1
-LLTprop = np.linalg.cholesky(covProp)
+    b0 = [2, -1, 2, 0]
+    nslvl = 0.2
+    nsp1 = 0.1
+    biasp1 = -1
 
-rndUs = [UnifVar([pl,ph]) for _ in range(3)]
-rnds = InvGaussVar(param=sinvg)
-obsvar = ObsVar(obs=ymes, prev_model=modelfit, cond_var=xmes)
+    xplot = np.repeat(np.c_[np.linspace(0,6,50)],2, axis=1)
+    xplot[:,1] = 1
 
-bstart = np.array([2.2,-0.7,1.5, 0.5])
+    xmes = np.hstack([np.c_[[0, 0.5, 1, 2, 2.5, 2.8, 4, 4.4, 5.2, 5.5]],
+                    biasp1+nsp1*np.c_[np.random.randn(10)]])
+    ymes = modeltrue(xmes, b0)
+    ymes += np.random.randn(xmes.shape[0])*nslvl
+
+if case == 1:
+    def ishigami(x,b):
+        return np.c_[np.sin(x[:,0]) + b[:,0]*np.sin(x[:,1])**2 +  \
+               b[:,1]*np.sin(x[:,0])*x[:,2]**4] 
+
+    Xtrain = sst.qmc.scale(sst.qmc.LatinHypercube(d=5).random(100),
+                            [-3, -3, -3, 4, 0.01], [3, 3, 3, 9, 0.3])
+    DXtrain = Xtrain.max(axis=0) - Xtrain.min(axis=0)
+    lowD = DXtrain/20
+    highD = DXtrain*5
+    ytrain = ishigami(Xtrain[:,:3],Xtrain[:,3:])
+
+    XX = np.array([[0,0,0],
+                [-1.3,0,-1],
+                [0.2,1,-1],
+                [2,1,-2],
+                [3.4,1,-1],
+                [2.5,-2,-1],
+                [3,3,-2],
+                [-1,-1,2],
+                [2.7,-2,0],
+                [1,1,-2]])
+    b3 = np.array([7,0.1])
+
+    kernel = RBF([1.0]*5, 
+                [(el1, el2) for el1,el2 in zip(lowD, highD)]) + \
+        WhiteKernel(noise_level=0.001,
+                    noise_level_bounds=(1e-6,np.var(ytrain)*0.05))
+
+    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=20)
+
+    gp.fit(Xtrain, ytrain)
+
+    def modelgp(x, b):
+        return gp.predict(np.hstack([x,
+                    np.repeat(np.r_[[b]],x.shape[0], axis=0)])).ravel()
+
+    ytrue = ishigami(XX,
+                np.repeat(np.atleast_2d(b3),XX.shape[0], axis=0)).ravel()
+    ypred = modelgp(XX, b3)
+    Q2emp = 1-np.var(ytrue-ypred)/np.var(ypred)
+            
+    if True:
+        fig, ax = plt.subplots()
+        ax.plot(XX[:,0], ytrue, 'or')
+        ax.plot(XX[:,0], ypred, '.b')
+        axx = ax.twiny()
+        axx.plot(ytrue, ypred, '+k')
+        axx.plot(ytrue, ytrue, '+-k')
+        ax.set_title('Q2emp=' + \
+                    str(np.round(Q2emp,3)))
+
+    ymes = ishigami(XX, np.repeat(np.r_[[b3]],XX.shape[0], axis=0)).ravel()
+
+#%%
+
+if case == 0:
+    Ndim = 3
+    pl = -3
+    ph = 3
+    sinvg = [0.4, 0.3, 1]
+
+    sm = [0.4, 0.4, 0.05]
+    smexp = 0.1
+    covProp = np.eye(3)*1e-1
+    LLTprop = np.linalg.cholesky(covProp)
+
+    rndUs = [UnifVar([pl,ph]) for _ in range(3)]
+    rnds = InvGaussVar(param=sinvg)
+    obsvar = ObsVar(obs=ymes, prev_model=modelfit, cond_var=xmes)
+
+    bstart = np.array([2.2,-0.7,1.5, 0.5])
+
+if case == 1:
+    Ndim = 2
+    sinvg = [0.4, 0.3, 1]
+
+    sm = [0.2, 5e-2]
+    smexp = 0.2
+    covProp = np.array([[0.2,0],[0,0.05]])
+    LLTprop = np.linalg.cholesky(covProp)
+
+    rndUs = [UnifVar([0,10]), UnifVar([0,1])]
+    rnds = InvGaussVar(param=sinvg)
+    obsvar = ObsVar(obs=ymes, prev_model=modelgp, cond_var=XX)
+
+    bstart = [2, 0., 0.5]
 
 
 #%%
 
 NMCMC = 20000
 
-MCalgo = MHalgo(NMCMC, Nthin=20, Nburn=2000)
-MCalgo.initialize(obsvar, rndUs, rnds, svar=sm, sdisc=smexp, Lblock=LLTprop)
+if inftype == 'MH':
+    MCalgo = MHalgo(NMCMC, Nthin=20, Nburn=5000, is_adaptive=True,
+                     verbose=True)
+if inftype == 'MHwG':
+    MCalgo = MHwGalgo(NMCMC, Nthin=20, Nburn=5000, is_adaptive=True,
+                       verbose=True)
+MCalgo.initialize(obsvar, rndUs, rnds, svar=sm, sdisc=smexp)
 MCalgo.MCchain[0] = bstart
 MCalgo.state(0, set_state=True)
-MCalgo.runInference()
-
-print(MCalgo.nacc/(MCalgo.N - MCalgo.Nburn))
-
-# nacc = 0
-# 
-# for i in range(1,MCalgo.N):
-#     MCalgo.move_prop(i=i, x0=MCalgo.MCchain[i-1], Lblock=LLTprop)
-#     llprop, lpprop = MCalgo.state(i)
-#     ldiff = llprop + lpprop - MCalgo.log_state - MCalgo.prior_state
-#     if ldiff > np.log(np.random.rand()):
-#         if i>MCalgo.Nburn: nacc += 1
-#         MCalgo.llchain[i] = llprop
-#         MCalgo.log_state = llprop
-#         MCalgo.prior_state = lpprop
-#     else: MCalgo.stay(i)
-#     if (i%1000 == 0) : print(i)
-#     if (i%500 == 0) & (i<=MCalgo.Nburn) :
-#         covProp = np.cov(MCalgo.MCchain[i-500:i,:MCalgo.Ndim].T) 
-#         LLTprop = np.linalg.cholesky(covProp * 2.38**2/(Ndim-1) +
-#                                     np.eye(Ndim)*1e-8)
-#         MCalgo.Lblock = LLTprop
-# 
-# print(nacc / (NMCMC*0.9))
-
+MCout, llout = MCalgo.runInference()
 
 #%%
 
-# for i in range(1,NMCMC):
-#     MCalgo.move_prop(i=i, x0=MCalgo.MCchain[i-1])
-#     llprop, lpprop = obsvar.loglike(xprop, rnds.diagSmat(s=sp, N=obsvar.Ndata))
-#     ldiff = llprop + lspp - llold - lsold
-#     if ldiff > np.log(np.random.rand()):
-#         if i>Nburn: naccmultiD[Ndim] += 1
-#         MCchain[i,Ndim] = sp
-#         llchain[i] = llprop
-#         llold = llprop
-#         lsold = lspp
-#     else:
-#         MCchain[i,Ndim] = MCchain[i-1,Ndim]
-#         llchain[i] = llchain[i-1]
-#     idj = np.random.permutation(Ndim)
-#     for j in idj:
-#         xprop[j] = rndUs[j].proposal(MCchain[i-1,j],sm[j])
-#         llprop = obsvar.loglike(xprop, rnds.diagSmat(s=sp, N=obsvar.Ndata))
-#         lpprop = rndUs[j].logprior(xprop[j])
-#         ldiff = llprop + lpprop - llold - lpold
-#         if ldiff > np.log(np.random.rand()):
-#             if i>Nburn: naccmultiD[j] += 1
-#             MCchain[i,j] = xprop[j]
-#             llchain[i] = llprop
-#             llold = llprop
-#             lpold = lpprop
-#         else:
-#             xprop[j] = MCchain[i-1,j]
-#             MCchain[i,j] = xprop[j]
-#             llchain[i] = llchain[i-1]
-#     if (i%1000 == 0): print(i)
+fig0, ax0 = MCalgo.post_visupar()
+fig1, ax1 = MCalgo.diag_chain(2)
 
-# # %%
-
-MCf = MCalgo.MCchain[MCalgo.Nburn::MCalgo.Nthin,:]
-llf = MCalgo.llchain[MCalgo.Nburn::MCalgo.Nthin]
-idsort = np.argsort(llf)
-MCchainS = MCf[idsort,:]
-llchainS = llf[idsort]
-Ntot = MCchainS.shape[0]
-MAP = MCchainS[-1,:]
-Nppost = Ntot
-postY = np.array([sst.norm(loc=modelfit(xmes, MCchainS[i,:Ndim]),
-                            scale=MCchainS[i,Ndim]).rvs(xmes.shape[0])
-                              for i in range(Ntot-Nppost, Ntot)])
-
-
-startchain = False
-
-fig, ax = plt.subplots(Ndim+1,Ndim+1, figsize=(8,8))
-for i in range(Ndim+1):
-    for j in range(Ndim+1):
-        if j>i:
-            if startchain:
-                ax[i,j].plot(MCalgo.MCchain[:MCalgo.Nburn,j],
-                              MCalgo.MCchain[:MCalgo.Nburn,i],'-k',
-                    linewidth=0.5)
-            ax[i,j].scatter(MCchainS[:,j], MCchainS[:,i],
-                             c=llchainS[:],
-                            marker='.', cmap='jet')
-            # ax[i,j].plot(b0[j], b0[i], 's', color='k', ms=10, alpha=0.4)
-            ax[i,j].plot(MAP[j], MAP[i], 'dk')
-        elif j == i:
-            ax[i,j].hist(MCchainS[:,j], edgecolor='k')
-            ax[i,j].set_xlabel('b' + str(i))
-        else:
-            ax[i,j].set_visible(False)
-
-plt.show()
-
-# %%
--- pyBI/inference.py
+++ pyBI/inference.py
@@ -1,27 +1,40 @@
 import numpy as np
+from itertools import combinations
+from warnings import warn
+from matplotlib.pyplot import subplots
+
+from scipy.stats import gaussian_kde
+
 from .base import rnvmultiD
 
 class InfAlgo():
-    def __init__(self, N, Nthin=None, Nburn=0.1):
+    def __init__(self, N, Nthin=None, Nburn=0.1, is_adaptive=True,
+                 verbose=True):
         self.Nburn = int(N*0.1) if Nburn == 0.1 else Nburn
         self.Nthin = Nthin if Nthin is not None else 1
         self.N = N + self.Nburn
         self.log_state = 0
         self.prior_state = 0
+        self.is_adaptive = is_adaptive
+        self.verbose = verbose
 
     def initialize(self, obsObj, varObj, discrObj, svar=1, sdisc=1,
-                    Lblock=1):
+                    Lblock=None):
         self.Ndim = len(varObj)
         self.varObj = varObj
         self.discrObj = discrObj
         self.obsObj = obsObj
+        self.ran_chain = False
         self.MCchain = np.zeros((self.N, self.Ndim+1))
         self.llchain = np.zeros(self.N)
-        self.svar = svar*np.ones(self.Ndim) if isinstance(1.,(int,float)) \
+        self.svar = svar*np.ones(self.Ndim) if isinstance(svar,(int,float)) \
             else svar
         self.sdisc = sdisc
-        self.Lblock = np.eye(self.Ndim) if \
-              Lblock is isinstance(1.,(int,float)) else Lblock
+        if isinstance(Lblock,(int,float)):
+            self.Lblock = np.eye(self.Ndim)
+        elif Lblock is None:
+            self.Lblock = np.diag(self.svar)
+        else: self.Lblock = Lblock
         self.MCchain[0,:self.Ndim] = [varObj[i].draw() 
                                       for i in range(len(varObj))]
         self.MCchain[0,self.Ndim] = discrObj.draw()
@@ -40,42 +53,119 @@ class InfAlgo():
             self.prior_state = prior_state
         else: return log_state, prior_state
     
-    def move_prop(self, i=None, x0=None, Lblock=None):
-        if x0 is not None:
-            if Lblock is None:
-                xprop = [self.varObj[k].proposal(x0[k], self.svar[k]) 
-                        for k in range(len(self.varObj))]
-            elif isinstance(Lblock, np.ndarray):
-                xprop = list(rnvmultiD(np.array(x0[:self.Ndim]), Lblock))
-            else:
-                xprop = list(rnvmultiD(np.array(x0[:self.Ndim]), self.Lblock))
+    def move_prop(self, x0=None, i=0, Lblock=None):
+        if Lblock is not None: Ltmp = Lblock # rather for testing purposes
+        else: Ltmp = self.Lblock# default behaviour
+        if x0 is not None: # default behaviour
+            xprop = list(rnvmultiD(np.array(x0[:self.Ndim]), Ltmp))
             sprop = self.discrObj.proposal(x0[self.Ndim], self.sdisc)
-            if i is not None:
-                self.MCchain[i,:self.Ndim] = xprop
-                self.MCchain[i,self.Ndim] = sprop
-            return xprop + [sprop]
-        else:
-            if i is None: i=0
-            xprop = [self.varObj[k].proposal(self.MCchain[i,k], self.svar[k]) 
-                     for k in range(len(self.varObj))]
+        else: # rather for visualisation purpose
+            xprop = list(rnvmultiD(np.array(self.MCchain[i,:self.Ndim]), Ltmp))
             sprop = self.discrObj.proposal(self.MCchain[i,self.Ndim],
                                             self.sdisc)
-            return xprop + [sprop]
+        return xprop + [sprop]
+    
+    def move_uni(self, di, x0):
+        xprop = list(x0[:self.Ndim])
+        sprop = x0[self.Ndim]
+        if di < self.Ndim:
+            xprop[di] = self.varObj[di].proposal(x0[di], self.svar[di])
+        else: sprop = self.discrObj.proposal(x0[di], self.sdisc)
+        return xprop + [sprop]
+    
+    def store(self, i, xprop, sprop):
+        self.MCchain[i,:self.Ndim] = xprop
+        self.MCchain[i,self.Ndim] = sprop
         
-    def stay(self, i):
-        self.MCchain[i] = self.MCchain[i-1]
-        self.llchain[i] = self.llchain[i-1]
+    def stay(self, i, j=None):
+        if j is None:
+            self.MCchain[i] = self.MCchain[i-1]
+            self.llchain[i] = self.llchain[i-1]
+        else: 
+            self.MCchain[i,j] = self.MCchain[i-1,j]
+            self.llchain[i] = self.log_state
+
+    def thin_and_sort(self):
+        if not self.ran_chain: 
+            self.ran_chain = True
+            self.raw_chain = self.MCchain
+            self.cut_chain = self.raw_chain[self.Nburn::self.Nthin]
+            self.cut_llchain = self.llchain[self.Nburn::self.Nthin]
+            self.sorted_indices = np.argsort(self.cut_llchain)
+            self.idx_chain = self.cut_chain[self.sorted_indices,:]
+            self.MAP = self.idx_chain[-1]
+            return self.idx_chain, self.cut_llchain[self.sorted_indices]
+        else: 
+            warn("Already ran and sorted")
+            return self.idx_chain, self.cut_llchain[self.sorted_indices]
+        
+    def post_obs(self, model=None):
+        Mtmp = model if model is not None else self.obsObj.prev_model
+        postY = np.zeros((int((self.N - self.Nburn)/self.Nthin),
+                          self.obsObj.Ndata))
+        for i in range(postY.shape[0]):
+            postY[i,:] = Mtmp(self.obsObj.cond_var,
+                             self.idx_chain[i,:self.Ndim]) + \
+                        np.random.randn()*self.idx_chain[i,self.Ndim]
+        return postY
+    
+    def post_visuobs(self, di=0):
+        fig, ax = subplots()
+        postY = self.post_obs()
+        ax.plot(self.obsObj.cond_var[:,di], postY.T, '.k')
+        ax.plot(self.obsObj.cond_var[:,di], self.obsObj.obs, 'or')
+        return fig, ax
+    
+    def post_visupar(self):
+        MC = self.idx_chain
+        LL = self.cut_llchain[self.sorted_indices]
+        quadrans = list(combinations(
+                ['v' + str(i) for i in range(self.Ndim)] + ['d'],2))
+        quad_idx = list(combinations(list(range(self.Ndim+1)),2))
+        Nbplot = int(np.ceil(np.sqrt(len(quadrans))))
+        k = 0
+        fig, ax = subplots(Nbplot, Nbplot)
+        for i in range(Nbplot):
+            for j in range(Nbplot):
+                if k<len(quadrans):
+                    ax[i,j].scatter(MC[:,quad_idx[k][0]], MC[:,quad_idx[k][1]],
+                                    c=LL, cmap='jet', marker='.')
+                    ax[i,j].set_xlabel(quadrans[k][0])
+                    ax[i,j].set_ylabel(quadrans[k][1])
+                    ax[i,j].plot(self.MAP[quad_idx[k][0]],
+                                  self.MAP[quad_idx[k][1]], 'dk')
+                    k += 1
+                else: ax[i,j].set_visible(False)
+        fig.tight_layout()
+        return fig, ax
+    
+    def diag_chain(self, di=0):
+        MC = self.cut_chain
+        Nc = int(self.cut_chain.shape[0]/4)
+        kdes = [gaussian_kde(MC[i*Nc:(i+1)*Nc,di]) for i in range(0,4)]
+        fig, ax = subplots(2,1)
+        ax[0].plot(self.raw_chain[:,di], '-k', lw=0.5)
+        ax[1].hist(self.idx_chain[:,di], edgecolor='k', alpha=0.6)
+        axx = ax[1].twinx()
+        xi = np.linspace(MC[:,di].min(), MC[:,di].max())
+        for i in range(4):
+            axx.plot(xi, kdes[i].pdf(xi), 'k')
+        return fig, ax
+
+
 
     
 
 class MHalgo(InfAlgo):
-    def __init__(self, N, Nthin=None, Nburn=0.1):
-        super().__init__(N, Nthin, Nburn)
+    def __init__(self, N, Nthin=None, Nburn=0.1, is_adaptive=True,
+                 verbose=True):
+        super().__init__(N, Nthin, Nburn, is_adaptive, verbose)
 
     def runInference(self):
         self.nacc = 0 
         for i in range(1,self.N):
-            self.move_prop(i=i, x0=self.MCchain[i-1], Lblock=self.Lblock)
+            prop = self.move_prop(self.MCchain[i-1])
+            self.store(i, prop[:self.Ndim], prop[self.Ndim])
             llprop, lpprop = self.state(i)
             ldiff = llprop + lpprop - self.log_state - self.prior_state
             if ldiff > np.log(np.random.rand()):
@@ -84,10 +174,46 @@ class MHalgo(InfAlgo):
                 self.log_state = llprop
                 self.prior_state = lpprop
             else: self.stay(i)
-            if (i%1000 == 0) : print(i)
-            if (i%500 == 0) & (i<=self.Nburn) :
+            if (i%1000 == 0) & (self.verbose) : print(i)
+            if (i%500 == 0) & (i<=self.Nburn) & (self.is_adaptive):
                 covProp = np.cov(self.MCchain[i-500:i,:self.Ndim].T) 
                 LLTprop = np.linalg.cholesky(covProp * 2.38**2/(self.Ndim-1) +
                                             np.eye(self.Ndim)*1e-8)
                 self.Lblock = LLTprop
-        return self.MCchain
+        MCS = self.thin_and_sort()
+        self.tacc = self.nacc/(self.N - self.Nburn)
+        if self.verbose: print('acceptation rate', self.tacc)
+        return MCS[0], MCS[1]
+    
+
+class MHwGalgo(InfAlgo):
+    def __init__(self, N, Nthin=None, Nburn=0.1, is_adaptive=True,
+                 verbose=False):
+        super().__init__(N, Nthin, Nburn, is_adaptive, verbose)
+
+    def runInference(self):
+        self.nacc = np.zeros(self.Ndim+1) 
+        for i in range(1,self.N):
+            self.MCchain[i] = self.MCchain[i-1]
+            id_rnd = np.random.permutation(self.Ndim+1)
+            for idx in id_rnd:
+                prop = self.move_uni(idx, self.MCchain[i])
+                self.store(i, prop[:self.Ndim], prop[self.Ndim])
+                llprop, lpprop = self.state(i)
+                ldiff = llprop + lpprop - self.log_state - self.prior_state
+                if ldiff > np.log(np.random.rand()):
+                    if i>self.Nburn: self.nacc[idx] += 1
+                    self.llchain[i] = llprop
+                    self.log_state = llprop
+                    self.prior_state = lpprop
+                else: self.stay(i,idx)
+            if (i%1000 == 0) & (self.verbose): print(i)
+            if (i%500 == 0) & (i<=self.Nburn) & (self.is_adaptive):
+                for idx in id_rnd:
+                    if idx != self.Ndim:
+                        self.svar[idx] = np.std(self.MCchain[i-500:i,idx])*1 
+        MCS = self.thin_and_sort()
+        self.tacc = self.nacc/(self.N - self.Nburn)
+        if self.verbose: print('acceptation rate', self.tacc)
+        return MCS[0], MCS[1]
+    
