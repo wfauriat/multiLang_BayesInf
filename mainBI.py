b0d29aea00ab42d6099d653e240dedf72d659513
William Fauriat <180698994+wfauriat@users.noreply.github.com>
2025-07-07 09:15:55 +0200

    test case harmonization

--- mainBI.py
+++ mainBI.py
@@ -23,19 +23,19 @@ np.set_printoptions(suppress=True, precision=5)
 # FOR DEVELOPMENT PURPOSES
 #
 
-import importlib
-import pyBI.base
-new_mod = importlib.reload(pyBI.base)
-GaussLike = new_mod.GaussLike
+# import importlib
+# import pyBI.base
+# new_mod = importlib.reload(pyBI.base)
+# GaussLike = new_mod.GaussLike
 
 # np.random.seed(123)
 ## REFAIRE LOG-LIKELIHOOD en distinguant MEAN(paramsA) et COV(paramsB)
 
 #%%
 
-case = 0
-# case = 1
-# case = 2
+casep = 0
+# casep = 1
+# casep = 2
 
 inftype = 'MH'
 # inftype = 'MHwG'
@@ -44,7 +44,7 @@ inftype = 'MH'
 # DEFINITION OF APPLICATION / CALIBRATION CASE
 ###############################################################################
 
-if case == 0:
+if casep == 0:
     def modeltrue(x,b):
         return np.atleast_2d(b[0] + b[1]*x[:,0] + b[2]*x[:,0]**2 + \
                              4*x[:,1] + 0.2*x[:,0]**3)
@@ -76,7 +76,7 @@ if case == 0:
     # gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=20)
     # gp.fit(xmes, ymes.ravel())
 
-if case == 1:
+if casep == 1:
     def ishigami(x,b):
         return np.atleast_2d(np.sin(x[:,0]) + b[:,0]*np.sin(x[:,1])**2 +  \
                b[:,1]*np.sin(x[:,0])*x[:,2]**4)
@@ -89,7 +89,7 @@ if case == 1:
                 [2,1,-2], [3.4,1,-1], [2.5,-2,-1],
                 [3,3,-2], [-1,-1,2], [2.7,-2,0],
                 [1,1,-2]])
-    b3 = np.array([7,0.1])
+    b0 = np.array([7,0.1])
 
     DXtrain = Xtrain.max(axis=0) - Xtrain.min(axis=0)
     lowD = DXtrain/20
@@ -107,8 +107,8 @@ if case == 1:
                     np.repeat(np.r_[[b]],x.shape[0], axis=0)])).ravel())
 
     ytrue = ishigami(XX,
-                np.repeat(np.atleast_2d(b3),XX.shape[0], axis=0))
-    ypred = modelgp(XX, b3)
+                np.repeat(np.atleast_2d(b0),XX.shape[0], axis=0))
+    ypred = modelgp(XX, b0)
     Q2emp = 1-np.var(ytrue-ypred)/np.var(ypred)
     ymes = ytrue
            
@@ -123,7 +123,9 @@ if case == 1:
                     str(np.round(Q2emp,3)))
     print('Q2emp =', Q2emp)
 
-if case == 2:
+    xmes = XX
+
+if casep == 2:
     Nsamp = 50
     mu_true = np.array([1,2,4])
     cor_true = np.array([[1,     0.8,  -0.1],
@@ -144,7 +146,7 @@ if case == 2:
 ###############################################################################
 
 ############## FULLY ANALYTICAL REGRESSION EXAMPLE
-if case == 0:
+if casep == 0:
     Ndim = 3
     pl = -5
     ph = 5
@@ -163,7 +165,7 @@ if case == 0:
                        [float(rnds.draw())])
     
 ############## ISHIGAMI AND GP MODEL SURROGATE FOR CALIBRATION
-if case == 1:
+if casep == 1:
     Ndim = 2
     sinvg = [0.4, 0, 2]
 
@@ -180,7 +182,7 @@ if case == 1:
                        [float(rnds.draw())])
     
 ############## STANDARD MULTIVARIATE PARAMETER ESTIMATION
-if case == 2:
+if casep == 2:
     Ndim = 3
     sinvg = [0.2, 0.2, 1]
     rndUs = [UnifVar([0,6]), UnifVar([0,6]), UnifVar([0,6])]
@@ -219,7 +221,6 @@ MCout, llout = MCalgo.runInference()
 MCalgo.post_visupar()
 MCalgo.hist_alldim()
 
-#%%
 print(MCalgo)
 
 
@@ -227,7 +228,7 @@ print(MCalgo)
 # VISUALISATION FOR PURE STATISTICAL INFERENCE
 ###############################################################################
 
-if case == 2:
+if casep == 2:
     print(sst.multivariate_normal(
         MCalgo.MAP[:3], rnds.diagSmat(MCalgo.MAP[3], 3)).logpdf(yrnd).sum())
     print(obsvar.loglike(MCalgo.MAP[:3],rnds.diagSmat(MCalgo.MAP[3], 3)))
@@ -240,282 +241,125 @@ if case == 2:
     ax.scatter(yrnd[:,0], yrnd[:,1], marker='o', color='b')
 
 
-
 #%%############################################################################
 # TEST OF FITTING GP 
 ###############################################################################
 
-# xmes = XX
-gpobj = HGP(xmes, ymes.ravel())
-# gpobj.bounds[-1] = [1e0, 1e1]
+if (casep == 0) | (casep == 1):
 
-gpobj.mtune(N=10)
+    if casep == 1:
+        del ishigami
+        def ishigami(x,b):
+            return np.atleast_2d(np.sin(x[:,0]) + b[0]*np.sin(x[:,1])**2 +  \
+                b[1]*np.sin(x[:,0])*x[:,2]**4)
 
-xtest = np.linspace(0.5,5.4,100)
-xtest = np.vstack([xtest, np.random.randn(xtest.shape[0])*nsp1+biasp1+0]).T
-
-ypred = gpobj.m_predict(xtest)
-spred = np.diag(gpobj.cov_predict(xtest, noise=True))
-ypred0 = gpobj.m_predict(xmes)
-spred0 = np.diag(gpobj.cov_predict(xmes, noise=True))
-
-fig, ax = plt.subplots(1,2, figsize=(9,4))
-ax[0].plot(xmes[:,0], ymes.ravel(), 'or')
-ax[0].plot(xmes[:,0], modeltrue(xmes, b0).ravel(), 'sk', alpha=0.2, ms=3)
-ax[0].plot(xmes[:,0], gpobj.m_predict(xmes), 'xb')
-ax[0].fill_between(x=xmes[:,0], y1=ypred0 + 2*spred0**0.5,
-                y2=ypred0 -2*spred0**0.5, alpha=0.2, color='b')
-ax[0].plot(xtest[:,0], ypred, 'x-m', ms=2, lw=0.5)
-ax[0].plot(xtest[:,0], modeltrue(xtest, b0).ravel(), 'sm', alpha=0.2, ms=3)
-ax[0].fill_between(x=xtest[:,0], y1=ypred + 2*spred**0.5,
-                y2=ypred -2*spred**0.5, alpha=0.2, color='m')
+        modeltrue = ishigami
 
 
-ax[1].plot(modeltrue(xtest, b0).ravel(), ypred, 'ob')
-ax[1].plot(ypred, ypred, '-k')
+    gpobj = HGP(xmes, ymes.ravel())
+    # gpobj.bounds[-1] = [1e0, 1e1]
+    gpobj.mtune(N=10)
 
-print(gpobj)
+    if casep == 0:
+        xtest = np.linspace(0.5,5.4,100)
+        xtest = np.vstack([xtest, np.random.randn(xtest.shape[0])*nsp1+biasp1+0]).T
+        ypred = gpobj.m_predict(xtest)
+        spred = np.diag(gpobj.cov_predict(xtest, noise=True))
 
-#%%
+    ypred0 = gpobj.m_predict(xmes)
+    spred0 = np.diag(gpobj.cov_predict(xmes, noise=True))
 
-# fig, ax = MCalgo.post_visuobs(0)
-# ax.plot(xtest[:,0], ypred, 'xm')
-# ax.plot(xtest[:,0], modeltrue(xtest, b0).ravel(), 'sm', alpha=0.2)
-# ax.fill_between(x=xtest[:,0], y1=ypred + 2*spred**0.5,
-#                 y2=ypred-2*spred**0.5, alpha=0.2, color='m')
-# ax.plot(xmes[:,0], gpobj.m_predict(xmes), 'xb')
+    fig, ax = plt.subplots(1,2, figsize=(9,4))
+    ax[0].plot(xmes[:,0], ymes.ravel(), 'or')
+    ax[0].plot(xmes[:,0], modeltrue(xmes, b0).ravel(), 'sk', alpha=0.2, ms=3)
+    ax[0].plot(xmes[:,0], gpobj.m_predict(xmes), 'xb')
+    ax[0].fill_between(x=xmes[:,0], y1=ypred0 + 2*spred0**0.5,
+                    y2=ypred0 -2*spred0**0.5, alpha=0.2, color='b')
+
+    if casep == 0:
+        ax[0].plot(xtest[:,0], ypred, 'x-m', ms=2, lw=0.5)
+        ax[0].plot(xtest[:,0], modeltrue(xtest, b0).ravel(), 'sm', alpha=0.2, ms=3)
+        ax[0].fill_between(x=xtest[:,0], y1=ypred + 2*spred**0.5,
+                    y2=ypred -2*spred**0.5, alpha=0.2, color='m')
+
+    if casep == 0:
+        ax[1].plot(modeltrue(xtest, b0).ravel(), ypred, 'ob')
+        ax[1].plot(ypred, ypred, '-k')
+
+    print(gpobj)
 
 
 #%%############################################################################
 # VERIFICATION OF GAUSSIAN LIKELIHOOD
 ###############################################################################
 
-btest = b0
-# btest = [-2, -1, 2]
-# btest = MCalgo.MAP
+if casep == 0:
 
-LLobj = GaussLike(ymes, gmod=modelfit, x=xmes,
-                   gpar=btest[:3], kpar=[1,1,1], spar=1)
-LLobj.setpar(gpar=MCalgo.MAP, kpar=[5,1,1], spar=1)
-# LLobj.gp_init()
-print(LLobj.loglike())
+    btest = b0
+    # btest = [-2, -1, 2]
+    # btest = MCalgo.MAP
 
+    LLobj = GaussLike(ymes, gmod=modelfit, x=xmes,
+                    gpar=btest[:3], kpar=[1,1,1], spar=1)
+    LLobj.setpar(gpar=MCalgo.MAP, kpar=[5,1,1], spar=1)
+    print(LLobj.loglike())
 
-# fig, ax = plt.subplots()
-# ax.plot(xmes[:,0], np.ravel(ymes), 'or')
-# ax.plot(xmes[:,0], np.ravel(LLobj.mean(xmes)), 'xb')
-# ax.plot(xmes[:,0], np.ravel(LLobj.mean(xmes)) + \
-#         2* np.diag(LLobj.cov(xmes)), '.b')
-# ax.plot(xmes[:,0], np.ravel(LLobj.mean(xmes)) - \
-#         2* np.diag(LLobj.cov(xmes)), '.b')
-# ax.plot(xtest[:,0], np.ravel(LLobj.mean(xtest)), 'x-m')
+    fig, ax = plt.subplots()
+    y0 = LLobj.g_predict(xmes).ravel()
+    y1 = LLobj.mgp.m_predict(xmes).ravel()
+    y2 = LLobj.mean(xmes).ravel()
+    ypostBI = MCalgo.post_obs()
+    ax.plot(xmes[:,0], ypostBI.T, '.k', ms=3)
+    ax.plot(xmes[:,0], ymes.ravel(), 'or', ms=8, label='observation')
+    ax.plot(xmes[:,0], y0, 'g', label='trend')
+    ax.plot(xmes[:,0], y1, 'm', label='gp correc, errmod')
+    ax.plot(xmes[:,0], y2 + 2*np.diag(LLobj.cov(xmes)), '.b', label='post uncty')
+    ax.plot(xmes[:,0], y2 - 2*np.diag(LLobj.cov(xmes)), '.b')
+    ax.plot(xmes[:,0], y0+y1, 'b')
+    ax.plot(xtest[:,0], np.ravel(LLobj.mean(xtest)), 'x-m', alpha=0.3)
 
-fig, ax = plt.subplots()
-y0 = LLobj.g_predict(xmes).ravel()
-y1 = LLobj.mgp.m_predict(xmes).ravel()
-y2 = LLobj.mean(xmes).ravel()
-ypostBI = MCalgo.post_obs()
-ax.plot(xmes[:,0], ypostBI.T, '.k', ms=3)
-ax.plot(xmes[:,0], ymes.ravel(), 'or', ms=8, label='observation')
-ax.plot(xmes[:,0], y0, 'g', label='trend')
-ax.plot(xmes[:,0], y1, 'm', label='gp correc, errmod')
-ax.plot(xmes[:,0], y2 + 2*np.diag(LLobj.cov(xmes)), '.b')
-ax.plot(xmes[:,0], y2 - 2*np.diag(LLobj.cov(xmes)), '.b')
-# ax.plot(xmes[:,0], y0+y1, 'b')
-ax.plot(xmes[:,0], y2 , '-xb', label='predicted mean')
+    ax.legend()
 
-ax.legend()
+#%%############################################################################
+# INFERENCE WITH GAUSSIAN LIKELIHOOD MADE OF GP + LINEAR REGRESSION
+###############################################################################
 
-#%%
+if casep == 0:
 
-Nburn2 = 10000
+    Nburn2 = 10000
 
+    pl = -5
+    ph = 5
+    sinvg = [0.2, -0.1, 2]
+    rndUs = [UnifVar([pl,ph]) for _ in range(3)]
+    # rndUs = [UnifVar([0.5,2.5]),UnifVar([-1.5,-0.5]), UnifVar([1.5,2.5]) ]
+    rnds = InvGaussVar(param=[0.05, -0.01, 2])
+    rndk = []
+    rndk.append(UnifVar([1e-1, 200]))
+    rndk.append(UnifVar([2e-1, 10]))
+    rndk.append(UnifVar([1e-2, 3]))
 
-pl = -5
-ph = 5
-sinvg = [0.2, -0.1, 2]
-rndUs = [UnifVar([pl,ph]) for _ in range(3)]
-# rndUs = [UnifVar([0.5,2.5]),UnifVar([-1.5,-0.5]), UnifVar([1.5,2.5]) ]
-rnds = InvGaussVar(param=[0.05, -0.01, 2])
-rndk = []
-rndk.append(UnifVar([1e-1, 200]))
-rndk.append(UnifVar([2e-1, 10]))
-rndk.append(UnifVar([1e-2, 3]))
+    lvarobj = {'gpar': [],
+            'kpar': [],
+            'spar': []}
 
-lvarobj = {'gpar': [],
-           'kpar': [],
-           'spar': []}
+    lvarobj['gpar'].extend(rndUs)
+    lvarobj['kpar'].extend(rndk)
+    lvarobj['spar'].append(rnds)
 
-lvarobj['gpar'].extend(rndUs)
-lvarobj['kpar'].extend(rndk)
-lvarobj['spar'].append(rnds)
+    svar2 = [[0.1, 0.1, 0.1], [10, 1, 0.1], [0.05]]
 
+    MCalgo2 = MHwGalgo2(NMCMC, Nthin=20, Nburn=Nburn2, is_adaptive=True,
+                        verbose=verbose)
+    MCalgo2.initialize(LLobj, lvarobj, svar2)
+    MCout2, llout2 = MCalgo2.runInference()
 
-svar2 = [[0.1, 0.1, 0.1], [10, 1, 0.1], [0.05]]
-
-MCalgo2 = MHwGalgo2(NMCMC, Nthin=20, Nburn=Nburn2, is_adaptive=True,
-                       verbose=verbose)
-MCalgo2.initialize(LLobj, lvarobj, svar2)
-MCout2, llout2 = MCalgo2.runInference()
-
-#%%
-
-fig, ax = MCalgo2.post_visupar()
-fig.tight_layout(pad=0)
-MCalgo2.hist_alldim()
-
-
-
-
-# vdim = np.sum([len(lvarobj[el]) for el in lvarobj.keys()])
-
-# print("\n".join([str(el) for el in lvarobj.values()]))
-
-# NMCMC = 30
-
-# MCchain = np.zeros((NMCMC, vdim)) # Initialisation to size on inference pb vdim
-
-# def draw_all_vars():
-#     rnd = np.zeros(vdim)        
-#     i = 0
-#     for el in lvarobj.keys():
-#         for k in range(len(lvarobj[el])):
-#             rnd[i] = lvarobj[el][k].draw()
-#             i+=1
-#     return rnd
-
-# def place_var():
-#     place_g = len(lvarobj['gpar'])
-#     place_k = len(lvarobj['kpar'])
-#     place_s = len(lvarobj['spar'])
-#     return place_g, place_g + place_k, place_g + place_k + place_s
-
-# def category_from_chain(x, i):
-#     gpar = list(x[i][:place_var()[0]])
-#     kpar = list(x[i][place_var()[0]:place_var()[1]])
-#     spar = list(x[i][place_var()[1]:place_var()[2]])
-#     return gpar, kpar, spar
-
-# MCchain[0,:] = draw_all_vars()
-
-# catElem = category_from_chain(MCchain, 0)
-# # print(*catElement)
-# LLobj.setpar(catElem[0], catElem[1], catElem[2])
-# # LLobj.setpar(gpar=btest[:3], kpar=[5,1,1], spar=0.5)
-# print("gpar : ", LLobj.gpar)
-# print("kpar : ", LLobj.kpar)
-# print("spar : ", LLobj.spar)
-
-# log_state = LLobj.loglike()
-# log_prior = [[lvarobj[el][k].logprior(catElem[j][k])
-#                for k in range(len(lvarobj[el]))] 
-#                  for (el,j) in zip(lvarobj.keys(),range(3))]
-# log_prior_sum = np.sum(list(chain(*log_prior)))
-
-# print("log state :", log_state)
-# print("log_prior :", log_prior_sum)
-
-# svar = [[0.1, 0.1, 0.1], [10, 1, 0.1], [0.05]]
-
-# def proposal_all():
-#     catProp = [[], [], []]
-#     for (el,j) in zip(lvarobj.keys(), range(3)):
-#         for k in range(len(lvarobj[el])):
-#             catProp[j].append(lvarobj[el][k].proposal(
-#                 catElem[j][k], svar[j][k]))
-#     return catProp
-
-# print()
-# print(np.hstack([np.c_[list(chain(*catElem))],
-#                   np.c_[list(chain(*proposal_all()))]]))
-
-
-
-
-
-
-# for i in range(NMCMC):
-#     MCchain[i,:] = np.array(list(chain(*[[float(lvarobj[el][k].draw()) 
-#                               for k in range(len(lvarobj[el]))]
-#                               for el in lvarobj.keys()])))
-            
-
-
-
-    # def initialize(self, obsObj: ObsVar, varObj: RandVar, discrObj: RandVar,
-    #                 svar: float = 1., sdisc: Optional[float] = None,
-    #                 Lblock: Optional[np.ndarray] = None):
-
+    fig, ax = MCalgo2.post_visupar()
+    fig.tight_layout(pad=0)
+    MCalgo2.hist_alldim()
 
 
 # %%
-
-# #%%
-# ## CODE TO VERIFY GP IMPLEMENTATION
-# # def fmod(x, beta):
-# #     return beta[0]*x + beta[1]*x**2
-# # fmod = modelfit
-# b0 = [2, -1, 2, 0]
-# theta = [1, 0.1]
-# sigma = 1
-# sn = 0.1
-# # theta = [3.17435, 4.24163]
-# # sigma = 39.76435
-# # sn = 0.01
-
-
-# xtest = np.linspace(0.5,5.4,20)
-# xtest = np.vstack([xtest, np.random.randn(xtest.shape[0])*0.1-1]).T
-
-# kpar = [sigma] + theta
-# spar = sn
-
-# def raXX(X, Y, scl=theta):
-#     diff = X[:, np.newaxis, :] - Y[np.newaxis, :, :]
-#     scaled_diff_sq = (diff / scl)**2
-#     squared_weighted_distance = np.sum(scaled_diff_sq, axis=2)
-#     return squared_weighted_distance 
-
-# def KaXX(X, theta=theta, sigma=[sigma, sn]):
-#     return sigma[0]**2 * np.exp(-raXX(X,X,theta)/2) + \
-#         np.eye(X.shape[0])*sigma[1]
-
-# def KaxX(x, X, theta=theta, sigma=[sigma, sn]):
-#     return sigma[0]**2 * np.exp(-raXX(x,X, theta)/2)
-
-# XX = xmes
-# k_XX = KaXX(xmes) + np.eye(xmes.shape[0])*1e-8
-# L_XX = np.linalg.cholesky(k_XX)
-# alpha = np.linalg.solve(L_XX.T, np.linalg.solve(L_XX, ymes.ravel()))
-
-# def m_predict(x):
-#     K_xX = KaxX(x, XX)
-#     mu_pred = K_xX @ alpha
-#     return mu_pred
-
-# def cov_predict(x):
-#     K_xX = KaxX(x, XX)
-#     v = np.linalg.solve(L_XX, K_xX.T)
-#     cov_pred = KaxX(x, x) - v.T @ v
-#     return cov_pred
-
-# ypred = m_predict(xtest)
-# spred = np.diag(cov_predict(xtest))
-
-# fig, ax = plt.subplots(1,2, figsize=(8,5))
-# ax[0].plot(xmes[:,0], ymes.ravel(), 'or')
-# ax[0].plot(xtest[:,0], modeltrue(xtest, b0).ravel(), 'sk', alpha=0.2)
-# ax[0].plot(xmes[:,0], m_predict(xmes), '.-b')
-# ax[0].plot(xtest[:,0], m_predict(xtest), 'xk')
-# ax[0].plot(xtest[:,0], ypred + 2*spred, '.m', ms=2)
-# ax[0].plot(xtest[:,0], ypred - 2*spred, '.m', ms=2)
-# ax[1].plot(xmes[:,1], ymes.ravel(), 'or')
-# ax[1].plot(xtest[:,1], modeltrue(xtest, b0).ravel(), 'sk', alpha=0.2)
-# ax[1].plot(xmes[:,1], m_predict(xmes), '.-b')
-# ax[1].plot(xtest[:,1], m_predict(xtest), 'xk')
-# ax[1].plot(xtest[:,1], ypred + 2*spred, '.m', ms=2)
-# ax[1].plot(xtest[:,1], ypred - 2*spred, '.m', ms=2)
-
 
 # from scipy.spatial.distance import cdist
 # print(cdist(xmes[:,0][:,None], xmes[:,0][:,None], metric='sqeuclidean'))
