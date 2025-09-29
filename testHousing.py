c2aaa6bc359d01f569bd5625680481af8f3be5e4
William Fauriat <180698994+wfauriat@users.noreply.github.com>
2025-09-29 13:15:03 +0200

    startBIhousing

--- testHousing.py
+++ testHousing.py
@@ -1,5 +1,6 @@
-#%%
-
+#%%###########################################################################
+## IMPORT PACKAGES
+##############################################################################
 from sklearn.datasets import fetch_california_housing
 import numpy as np
 import scipy.stats as sst
@@ -10,9 +11,33 @@ import matplotlib.pyplot as plt
 
 from PIL import Image
 
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.svm import SVR
+
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.gaussian_process.kernels import WhiteKernel
+
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+
+
 np.set_printoptions(suppress=True, precision=4)
 
-#%%
+
+#%%###########################################################################
+## LOADING DATA AND MAP AND PREPROCESSING
+##############################################################################
 
 # Number of Instances:    20640
 # Number of Attributes:   8 numeric, predictive attributes and the target
@@ -30,9 +55,6 @@ np.set_printoptions(suppress=True, precision=4)
 #        'medianHouseValue']
 
 img = Image.open('cal_map.jpg')
-
-
-#%%
 
 # housing = fetch_california_housing()
 # df = pd.DataFrame(housing.data, columns=housing.feature_names)
@@ -57,8 +79,9 @@ censust = np.ravel([all(el) for el in census.T])
 valcens = df.values[censust,:]
 dfcens = pd.DataFrame(valcens, columns=df.columns)
 
-# %%
-
+#%%###########################################################################
+## VISUALISATION ON MAP
+##############################################################################
 dim = 8
 
 filtsorted = dfcens.values[np.argsort(dfcens.iloc[:,dim]),:]
@@ -70,18 +93,12 @@ cmap = ax.scatter((filtsorted[:,0]+120)*70+354,
 fig.colorbar(cmap, ax=ax, label=df.columns[dim])
 
 
-# # %%
-
-# fig, ax = plt.subplots()
-# cmap = ax.scatter(dfcens['medianIncome'].values,
-#                    dfcens['medianHouseValue'].values, 
-#                   c=dfcens['totalRooms'].values, 
-#            marker='.')
-# fig.colorbar(cmap, ax=ax, label='totalRooms')
 
 
-# %%
-# HISTOGRAMS
+
+#%%###########################################################################
+## HISTOGRAMS
+##############################################################################
 
 dims = len(dfcens.columns)
 
@@ -96,65 +113,50 @@ for i in range(int(dims/2),dims-1):
     ax[1,i-int(dims/2)].set_xlabel(dfcens.columns[i])
 fig.tight_layout()
 
-# %%
-# 1D REGRESSIONS
+#%%###########################################################################
+## 1D REGRESSIONS
+##############################################################################
+# dims = len(dfcens.columns)
 
-dims = len(dfcens.columns)
+# fig, ax = plt.subplots(2,int(dims/2), figsize=(10,4))
+# for i in range(int(dims/2)):
+#     sns.regplot(x=dfcens.iloc[:,i],  y=dfcens['medianHouseValue'],
+#                 line_kws={'color':'r'}, ax=ax[0,i])
+#     ax[0,i].set_xlabel(dfcens.columns[i])
+# for i in range(int(dims/2),dims-1):
+#     sns.regplot(x=dfcens.iloc[:,i],  y=dfcens['medianHouseValue'],
+#                 line_kws={'color':'r'}, ax=ax[1,i-int(dims/2)])
+#     ax[1,i-int(dims/2)].set_xlabel(dfcens.columns[i])
+# fig.tight_layout()
 
-fig, ax = plt.subplots(2,int(dims/2), figsize=(10,4))
-for i in range(int(dims/2)):
-    sns.regplot(x=dfcens.iloc[:,i],  y=dfcens['medianHouseValue'],
-                line_kws={'color':'r'}, ax=ax[0,i])
-    ax[0,i].set_xlabel(dfcens.columns[i])
-for i in range(int(dims/2),dims-1):
-    sns.regplot(x=dfcens.iloc[:,i],  y=dfcens['medianHouseValue'],
-                line_kws={'color':'r'}, ax=ax[1,i-int(dims/2)])
-    ax[1,i-int(dims/2)].set_xlabel(dfcens.columns[i])
-fig.tight_layout()
-
-# %%
-# TEST WITH VARIOUS MODELS
-
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import ElasticNet
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.svm import SVR
-
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-from sklearn.gaussian_process.kernels import WhiteKernel
-
-from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
-from sklearn.preprocessing import StandardScaler
-
+#%%###########################################################################
+## TEST WITH VARIOUS REGRESSION MODELS
+##############################################################################
 
 dims = len(dfcens.columns)
 XX = dfcens.values[:,:dims-1]
 # XX = np.float32(dfcens.values[:,2:-1])
 yy = np.float32(dfcens.values[:,-1])
 
-dimtrain = XX.shape[1]
-
 scaler_X = StandardScaler()
 scaler_y = StandardScaler()
 
 X_train, X_test, y_train, y_test = train_test_split(
-    XX, yy, test_size=0.8)
+    XX, yy, test_size=0.95)
+
+X_lat_test = X_test[:,:2]
+# X_train = X_train[:,2:] # to remove learning on latitude / longitude
+# X_test = X_test[:,2:] # to remove learning on latitude / longitude
+
+dimtrain = X_test.shape[1]
+
 X_train_scaled = scaler_X.fit_transform(X_train)
 y_train_scaled = scaler_y.fit_transform(y_train.reshape(-1, 1)).ravel()
 X_test_scaled = scaler_X.transform(X_test)
 
 # model = Ridge(alpha=0.01)
-# model = LinearRegression()
-model = ElasticNet()
+model = LinearRegression()
+# model = ElasticNet()
 # model = DecisionTreeRegressor()
 # model = RandomForestRegressor()
 # model = GradientBoostingRegressor()
@@ -203,9 +205,11 @@ mue4, sigmae4 = sst.norm.fit(np.abs(residuals[(y_test>300000) & (y_test<400000)]
 mue5, sigmae5 = sst.norm.fit(np.abs(residuals[(y_test>400000)]))
 
 
-#%%
+#%%###########################################################################
+## VISUALISATION OF FIT AND RESIDUALS
+##############################################################################
 
-vdim = 5
+vdim = -1
 nplot = 30
 visu_samp = np.random.randint(X_test.shape[0],size=nplot)
 
@@ -242,49 +246,70 @@ ax1.set_ylabel('')
 
 print(f"R-squared score on test set: {r2:.2f}")
 print(f"Std of error is : {sigmae:.2f}")
+print(f"Std of abs error in [0, 100000k$] : {sigmae1:.2f}")
+print(f"Std of abs error in [100000k$, 200000k$] : {sigmae2:.2f}")
+print(f"Std of abs error in [200000k$, 300000k$] : {sigmae3:.2f}")
+print(f"Std of abs error in [300000k$, 400000k$] : {sigmae4:.2f}")
+print(f"Std of abs error in [400000k$, 500000k$] : {sigmae5:.2f}")
 
 
-# %%
 
-filtsorted = y_pred[np.argsort(y_pred)]
+#%%###########################################################################
+## VISUALISATION OF PREDICTION AND RESIDUALS ON MAP
+##############################################################################
+
+idsort = np.argsort(y_test) 
+
+filtsorted = y_test[idsort]
 fig, ax = plt.subplots()
 ax.imshow(img)
-cmap = ax.scatter((X_test[:,0]+120)*70+354,
-                  -(X_test[:,1]-38)*70+312, c=filtsorted,
-            cmap='jet', marker='.', s=10, alpha=1)
+cmap = ax.scatter((X_lat_test[idsort,0]+120)*70+354,
+                  -(X_lat_test[idsort,1]-38)*70+312, c=filtsorted,
+            cmap='jet', marker='.', s=10, alpha=1, vmin=0)
 fig.colorbar(cmap, ax=ax, label=df.columns[8])
 
-# %%
+idsort = np.argsort(y_pred) 
 
-
-filtsorted = np.abs(residuals[np.argsort(np.abs(residuals))])
+filtsorted = y_pred[idsort]
 fig, ax = plt.subplots()
 ax.imshow(img)
-cmap = ax.scatter((X_test[:,0]+120)*70+354,
-                  -(X_test[:,1]-38)*70+312, c=filtsorted,
-            cmap='jet', marker='.', s=10, alpha=1)
+cmap = ax.scatter((X_lat_test[idsort,0]+120)*70+354,
+                  -(X_lat_test[idsort,1]-38)*70+312, c=filtsorted,
+            cmap='jet', marker='.', s=10, alpha=1, vmin=0)
 fig.colorbar(cmap, ax=ax, label=df.columns[8])
-# # %%
 
-# # TEST BI
+idsort = np.argsort(np.abs(residuals))
 
-# from itertools import chain
+filtsorted = np.abs(residuals[idsort])
+fig, ax = plt.subplots()
+ax.imshow(img)
+cmap = ax.scatter((X_lat_test[idsort,0]+120)*70+354,
+                  -(X_lat_test[idsort,1]-38)*70+312, c=filtsorted,
+            cmap='jet', marker='.', s=10, alpha=1, vmin=0)
+fig.colorbar(cmap, ax=ax, label='abs error')
 
-# from pyBI.base import UnifVar, InvGaussVar, ObsVar
-# from pyBI.base import HGP, GaussLike
-# from pyBI.inference import MHalgo, MHwGalgo, InfAlgo, InfAlgo2, MHwGalgo2
+
+#%%###########################################################################
+## TEST OF BAYESIAN INFERENCE
+##############################################################################
+
+from itertools import chain
+
+from pyBI.base import UnifVar, InvGaussVar, ObsVar
+from pyBI.base import HGP, GaussLike
+from pyBI.inference import MHalgo, MHwGalgo, InfAlgo, InfAlgo2, MHwGalgo2
 
 
-# Ndim = 9
-# sinvg = [0.2, -0.1, 100000]
+Ndim = 8
+sinvg = [0.2, -0.1, 100000]
 
-# sm = [10]*Ndim
-# smexp = 10
-# covProp = np.eye(Ndim)*1e-1
-# LLTprop = np.linalg.cholesky(covProp)
+sm = [10]*Ndim
+smexp = 10
+covProp = np.eye(Ndim)*1e0
+LLTprop = np.linalg.cholesky(covProp)
 
 # rndUs = [UnifVar([-30000,30000]),
-#     UnifVar([-30000,0]), UnifVar([-30000,0]),
+#          UnifVar([-30000,30000]),
 #          UnifVar([-2000,2000]),
 #          UnifVar([-2000,2000]),
 #          UnifVar([-2000,2000]),
@@ -292,31 +317,49 @@ fig.colorbar(cmap, ax=ax, label=df.columns[8])
 #          UnifVar([-30000,20000]),
 #          UnifVar([0,50000])
 # ]
-# def modelfit(x,b):
-#     return np.atleast_2d(b[0] + \
-#                          b[1]*x[:,1] +
-#                          b[2]*x[:,2] +
-#                          b[3]*x[:,3] +
-#                          b[4]*x[:,4] +
-#                          b[5]*x[:,5] +
-#                          b[6]*x[:,6] +
-#                          b[7]*x[:,7])[0]
+rndUs = [UnifVar([-50000,50000]),
+         UnifVar([-50000,50000]),
+         UnifVar([-20000,20000]),
+         UnifVar([-20000,20000]),
+         UnifVar([-20000,20000]),
+         UnifVar([-200,200]),
+         UnifVar([-30000,30000]),
+         UnifVar([-50000,50000])
+]
 
-# rnds = InvGaussVar(param=sinvg)
-# obsvar = ObsVar(obs=np.r_[y_train], prev_model=modelfit, cond_var=X_train)
+def modelfit(x,b):
+    return np.atleast_2d(b[0] + \
+                         b[1]*x[:,1] +
+                         b[2]*x[:,2] +
+                         b[3]*x[:,3] +
+                         b[4]*x[:,4] +
+                         b[5]*x[:,5] +
+                         b[6]*x[:,6] +
+                         b[7]*x[:,7])[0][0]
 
-# bstart = np.array([rndUs[i].draw() for i in range(Ndim)] + \
-#                     [float(rnds.draw())])
+rnds = InvGaussVar(param=sinvg)
 
-# NMCMC = 20000
-# Nburn = 5000
-# verbose = True
+bstart = np.array([rndUs[i].draw() for i in range(Ndim)] + \
+                    [float(rnds.draw())])
 
-# MCalgo = MHwGalgo(NMCMC, Nthin=20, Nburn=Nburn, is_adaptive=True,
-#                     verbose=verbose)
+obsvar = ObsVar(obs=np.c_[y_train], prev_model=modelfit, cond_var=X_train)
 
-# MCalgo.initialize(obsvar, rndUs, rnds)
-# MCalgo.MCchain[0] = bstart
-# MCalgo.state(0, set_state=True)
-# MCout, llout = MCalgo.runInference()
+NMCMC = 20000
+Nburn = 5000
+verbose = True
 
+MCalgo = MHwGalgo(NMCMC, Nthin=20, Nburn=Nburn, is_adaptive=True,
+                    verbose=verbose)
+
+
+tmp = obsvar.loglike(bstart[:Ndim],
+                     rnds.diagSmat(s=bstart[Ndim],
+                                N=obsvar.dimdata))
+MCalgo.initialize(obsvar, rndUs, rnds)
+MCalgo.sdisc = 10
+MCalgo.MCchain[0] = bstart
+MCalgo.state(0, set_state=True)
+MCout, llout = MCalgo.runInference()
+
+
+# %%
