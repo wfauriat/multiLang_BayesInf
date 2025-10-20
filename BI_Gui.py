7e8e5a366caad834102260ee40c40743f3dd2a6f
William Fauriat <180698994+wfauriat@users.noreply.github.com>
2025-10-20 14:50:41 +0200

    raw MVC arch

--- BI_Gui.py
+++ BI_Gui.py
@@ -1,12 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic 
+from PyQt5.QtWidgets import QApplication
+# from PyQt5 import uic 
 
-from UIcomps.componentsGUI import MyMainWindow
+from UIcomps.componentsGUI import ViewMainUI
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MyMainWindow()
+    window = ViewMainUI()
     # window = uic.loadUi("./UIcomps/BayesRegGUI.ui")
     window.show()
     sys.exit(app.exec())
