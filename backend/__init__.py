a5b39a6cc96275d92d60c03364ebc5794111b62e
william <180698994+wfauriat@users.noreply.github.com>
2025-12-01 00:53:29 +0100

    added static serve to flask

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import send_file, render_template
+
 
 from . import model
 
+import os
+
+template_dir = os.path.abspath('../frontend/build') 
+static_dir = os.path.abspath('../frontend/build/static')
+
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+    static_folder=static_dir,
+    template_folder=template_dir)
     CORS(app) # proxy can be used on the front side to prevent CORS issue
     # put "proxy": "http://127.0.0.1:5000", in package.json (on the front size)
 
@@ -18,5 +28,11 @@ def create_app():
     @app.route('/')
     def home():
         return "Hello, BI_Flask!\n"
+    
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve_frontend(path):
+        """Serve the main index.html file for any non-API route."""
+        return render_template('index.html')
     
     return app
